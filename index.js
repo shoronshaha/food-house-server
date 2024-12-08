4ac9f4bf593d1269b2c69733262ac007c9592e8f
Smaran Shaha <smaranshaha@gmail.com>
2024-12-08 15:52:17 +0600

    added cart to the mongdb and find to the client side

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db("foodHouse").collection("menu");
     const reviewsCollection = client.db("foodHouse").collection("reviews");
+    const cartCollection = client.db("foodHouse").collection("carts");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -35,6 +36,17 @@ async function run() {
     });
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
