58b77afef59b625006a7f5a8a6de6962618572d9
Smaran Shaha <smaranshaha@gmail.com>
2024-12-15 22:48:00 +0600

    some correction

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("foodHouse").collection("users");
     const menuCollection = client.db("foodHouse").collection("menu");
@@ -311,10 +311,10 @@ async function run() {
       res.send(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
